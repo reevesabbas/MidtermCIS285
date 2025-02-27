3a9806e883ce23e997824947801280d31c3acc8a
Reeves <reeves.abbas@gmail.com>
2025-02-27 14:18:20 -0500

    q2 commit2

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,6 +1,6 @@
 public class factorialcalculator {
   public static void main(String[] args) {
-      int num = 6;  // Predefined number to calculate factorial
+      int num = 2;  // Predefined number to calculate factorial
       int factorial = 3;
 
       for (int i = 1; i <= num; i++) { // Loop through numbers
