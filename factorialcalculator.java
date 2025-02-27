1f9dd6eda18a2067a042e5572fe2a9490be231c0
Reeves <reeves.abbas@gmail.com>
2025-02-27 14:19:21 -0500

    q2 commit3

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,7 +1,7 @@
 public class factorialcalculator {
   public static void main(String[] args) {
       int num = 2;  // Predefined number to calculate factorial
-      int factorial = 3;
+      int factorial = 5;
 
       for (int i = 1; i <= num; i++) { // Loop through numbers
           factorial *= i;  // Multiply current number
